cf56ed622c6b1e7be0602a92270b6bf1ba0413a8
felipe_oliveira2019@hotmail.com <felipe_oliveira2019@hotmail.com>
2025-10-29 08:16:56 -0300

    adicionando os arquivos adicionais

---
+++ js/code.js
@@ -0,0 +1,8 @@
+const ButtonAdd = document.querySelector(".btn_out ")
+const adicionar = () => {
+    console.log(ButtonAdd) 
+    ButtonAdd.addEventListener("click", () => { 
+        console.log("ola")
+    })
+}
+adicionar()
